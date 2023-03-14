369b74c340f34117e31e41609846fe4f2fbe40e7
Faik Hakan Bilgen <faikb@microsoft.com>
2023-03-14 12:39:40 +0300

    Add project files.

---
+++ RunDD/Jobs.cs
@@ -0,0 +1,117 @@
+﻿using DebugDiag.DotNet;
+using DebugDiag.DotNet.AnalysisRules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace RunDD
+{
+    internal class AnalysisJobBase
+    {
+
+        public Guid JobID { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        public ApartmentState AptState { get; set; }
+        public string FileName { get; set; }
+        public string ReportFileName { get; set; }
+        public List<string> FileNames { get; set; }
+
+        public string ComputerName { get; set; }
+        public byte[] ReportFileContent { get; set; }
+
+        public AnalysisJobBase()
+        {
+        }
+
+        public virtual void Analyze(object Data)
+        {
+            return;
+        }
+
+        public virtual void Initialize()
+        {
+            return;
+        }
+    }
+
+    internal class AnalysisJob : AnalysisJobBase
+    {
+
+        private string SymbolPath { get; set; }
+        private string ReportingPath { get; set; }
+        private string AnalysisRulePath { get; set; }
+        //private LogWriter Logger { get; set; }        
+
+        public AnalysisJob(string dumpPath, string symPath, string rptPath, string anlysRulePath)
+        {
+            FileName = dumpPath;
+            SymbolPath = symPath;
+            ReportingPath = rptPath;
+            AnalysisRulePath = anlysRulePath;
+     
+        }
+
+        public void StartJob()
+        {
+            Thread thread = null;
+
+            try
+            {
+                thread = new Thread(new ParameterizedThreadStart(Analyze));
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start(null);
+                thread.Join();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("ERROR : {0} ", Properties.Resource.EC200, ex.Message));
+            }
+
+        }
+
+        public override void Analyze(object Data)
+        {
+            bool IncludeSourceAndLineInformationInAnalysisReports = false;
+            bool SetContextOnCrashDumps = false;
+            bool DoHangAnalysisOnCrashDumps = false;
+            bool IncludeHttpHeadersInClientConns = false;
+            bool IncludeInstructionPointerInAnalysisReports = false;
+            bool ExcludeIdenticalStacks = false;
+            List<string> df = new List<string>();
+
+            var o = Console.Out;
+
+            using (NetAnalyzer netAnalyzer = new NetAnalyzer())
+            {
+                try
+                {
+                    Console.SetOut(TextWriter.Null);
+                    netAnalyzer.Initialize(IncludeSourceAndLineInformationInAnalysisReports, SetContextOnCrashDumps, DoHangAnalysisOnCrashDumps, IncludeHttpHeadersInClientConns, ExcludeIdenticalStacks, IncludeInstructionPointerInAnalysisReports);
+                    df.Add(FileName);
+                    netAnalyzer.AddDumpFiles(df, SymbolPath);
+                    netAnalyzer.AddAnalysisRulesToRunList(AnalysisRulePath, false);
+                    netAnalyzer.RunAnalysisRules(null, SymbolPath, "", ReportingPath, AnalysisModes.Unattended);
+
+                    while (!netAnalyzer.ReportReady)
+                        System.Threading.Thread.Sleep(1000);
+
+                    // We need to understand when netanalyzer produces multiple reports. Currently we rely on the first non-null report name !!!
+                    for (int i = 0; i != netAnalyzer.ReportFileNames.GetLength(0); i++)
+                        if (netAnalyzer.ReportFileNames[i] != null)
+                            ReportFileName = netAnalyzer.ReportFileNames[i];
+
+                    Console.SetOut(o);
+                }
+                catch (Exception ex)
+                {
+                    Console.SetOut(o);
+                    Console.WriteLine(string.Format("ERROR : {0} ", Properties.Resource.EC201, ex.Message));
+                }
+            }
+
+        }
+
+    }
+}
---
+++ RunDD/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Configuration;
+
+namespace RunDD
+{
+    internal struct Paths
+    {
+        public string filePath { get; set; }
+        public string reportPath { get; set; }
+        public string symbolPath { get; set; }
+
+        public Paths(string fp, string rp, string sp)
+        {
+            filePath = fp;
+            reportPath = rp;
+            symbolPath = sp;
+        }
+    }
+
+    internal class Program
+    {
+
+        private static AnalysisJob InitializeJob(Paths paths)
+        {
+            if (String.IsNullOrEmpty(paths.filePath))
+            {
+                Console.WriteLine($"The path to the dump file should be specified!");
+                return null;
+            }
+
+            if (String.IsNullOrEmpty(paths.reportPath))
+            {
+                Console.WriteLine($"The report file path is empty. Using {Environment.CurrentDirectory}");
+                paths.reportPath = Environment.CurrentDirectory;
+            }
+
+            if (String.IsNullOrEmpty(paths.symbolPath))
+            {
+                paths.symbolPath = ConfigurationManager.AppSettings["SymbolPath"].ToString();
+                Console.WriteLine($"The symbol file path is empty. Using {paths.symbolPath}");
+            }
+
+            var strRulePath = ConfigurationManager.AppSettings["AnalysisRulePath"].ToString();
+
+
+            return new AnalysisJob(paths.filePath, paths.symbolPath, paths.reportPath, strRulePath);
+        }
+
+        private static AnalysisJob Initialize(Paths paths)
+        {
+
+            return InitializeJob(paths);
+        }
+
+        private static void DisplayUsage()
+        {
+            Console.WriteLine();
+
+        }
+
+        private static Paths ProcessArguments(string[] args)
+        {
+            string filePath = "", reportPath = "", symbolPath = "";
+
+            for (int i = 0; i != args.Length; i++)
+            {
+                switch (args[i].Trim())
+                {
+                    case "-f":
+                        filePath = args[i + 1];
+                        break;
+                    case "-r":
+                        reportPath = args[i + 1];
+                        break;
+                    case "-s":
+                        symbolPath = args[i + 1];
+                        break;
+                    case "-h":
+                        DisplayUsage();
+                        break;
+                    case "-?":
+                        DisplayUsage();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return new Paths(filePath, reportPath, symbolPath);
+
+        }
+
+        private static void Analyze(AnalysisJob aj)
+        {
+            aj.StartJob();
+        }
+
+        static void Main(string[] args)
+        {
+            Analyze(Initialize(ProcessArguments(args)));
+        }
+    }
+}
